56521b16d7c75a7cd2e1cecfb18780e9e22396de
Robert Penicka <penicrob@fel.cvut.cz>
2020-05-11 10:50:31 +0200

    add installation of gitman

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -8,16 +8,20 @@ eval "$(ssh-agent -s)"
 chmod 600 ./.ci/deploy_key_github
 ssh-add ./.ci/deploy_key_github
 sudo apt-get update -qq
-sudo apt-get install dpkg git python-setuptools python3-setuptools python3-pip
+sudo apt-get install dpkg git python-setuptools python3-setuptools python-pip python3-pip python-pip
+
+sudo pip3 install gitman
+sudo -H pip3 install gitman
 
 echo "clone uav_core"
 cd
 git clone git@github.com:ctu-mrs/uav_core.git
 cd uav_core
+gitman install
 
 echo "clone simulation"
 cd 
 git clone git@github.com:ctu-mrs/simulation.git 
-gitman update
+gitman install
 
 echo "install part ended"
