ce5b431a55a5b982f923558951241892f2c9c55c
Robert Penicka <penicrob@fel.cvut.cz>
2020-05-11 12:33:20 +0200

    add index.md travis ci from uav_core

--- .ci/ci_test_build.sh
+++ .ci/ci_test_build.sh
@@ -7,6 +7,16 @@ cd $TRAVIS_BUILD_DIR/docs/software/
 mkdir mkdir -p uav_core
 cd uav_core
 
+README_FILE="~/uav_core/README.md"
+echo "$README_FILE"
+if [[ -f "$README_FILE" ]]; then
+    echo "processing uav_core"
+    echo -e "---\nlayout: default\ntitle: $module_name\nparent: Software\n---" > index.md
+    cat $README_FILE >> index.md
+else
+    echo "readme file $README_FILE does not exists"
+done
+
 for module in ~/uav_core/ros_packages/* ; do
     if [ -d "$module" ]; then
         README_FILE="$module/README.md"
