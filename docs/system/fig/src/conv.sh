86879133641049469a5569c247eab53057c6c65c
Matouš Vrba <vrbamato@MyslPlacka>
2020-07-25 13:49:04 +0200

    [relative_commands]: fixed fonts in svg figs

--- docs/system/fig/src/conv.sh
+++ docs/system/fig/src/conv.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
-for f in build/*.pdf; do
-  outf="${f%.pdf}.svg"
+
+for t in *.tex; do
+  latex -output-directory=build "$t" > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Warning: the command 'latex -output-directory=build $t' failed!"
+  fi
+done
+
+for f in build/*.dvi; do
+  outf="${f%.dvi}.svg"
   echo in: $f, out: $outf
-  inkscape -l "$outf" "$f"
-  cp "$outf" ../
+  dvisvgm --no-fonts "$f" -o "$outf" > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Warning: the command 'dvisvgm --no-fonts $f $outf' failed!"
+  fi
+  mv "$outf" ../
 done
