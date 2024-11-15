6670df876195336ba5a305382e748a9d4284e10b
Tomas Baca <klaxalk@gmail.com>
2024-11-15 17:15:27 +0100

    updated images

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -98,7 +98,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/mrs_logo.png',
     navbar: {
       title: 'CTU MRS Docs',
       logo: {
